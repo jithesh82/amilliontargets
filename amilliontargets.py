1f52298260e9ea7c13c9f4be8d05b8e3f3f44c87
jithesh82 <jithesh82@gmail.com>
2026-02-28 15:56:37 -0500

    update amilliontargets with timer

--- amilliontargets.py
+++ amilliontargets.py
@@ -5,11 +5,13 @@ Task2: add tqdm
 import asyncio
 from curl_cffi.requests import AsyncSession 
 import random
+import time
 
 urls =['www.tesla.com', 'www.war.gov', 'www.clio.com', 'www.criteo.com', 'account.t-mobile.com']
-urls = random.choices(urls, k=1)
+urls = random.choices(urls, k=5)
 
 async def main():
+    start = time.perf_counter()
     async with AsyncSession() as s:
         tasks = [s.get(url, impersonate='chrome110') for url in urls]
 
@@ -19,6 +21,10 @@ async def main():
             print(result.url, result.status_code)
             print(result.text, file=open('test' + '.html', 'w'))
 
+        print(results)
+
+        midtime = time.perf_counter()
+
         async def contentWrite(result):
             fName = result.url.split('/')[2] + '.html'
             with open(fName, 'w') as f:
@@ -27,7 +33,8 @@ async def main():
         tasks = [contentWrite(result) for result in results]
 
         results = await asyncio.gather(*tasks)
+    end = time.perf_counter()
+    print('time taken: %.2f, %.2f' % ((midtime - start), (end - midtime)))
 
-        print(results)
 
 asyncio.run(main())
