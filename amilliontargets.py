6bdb8fa68d80bce4bde5db376bada7f83a0d17d8
jithesh82 <jithesh82@gmail.com>
2026-02-28 23:47:41 -0500

    update amilliontargets.py

--- amilliontargets.py
+++ amilliontargets.py
@@ -1,24 +1,31 @@
 """
-Task1: save the html file to disk
-Task2: add tqdm 
+task1: parse html file for .js files
+taks2: recursively get all .js files
 """
 import asyncio
 from curl_cffi.requests import AsyncSession 
 import random
 import time
+#from pdb import set_trace as trace
 
-urls =['www.tesla.com', 'www.war.gov', 'www.clio.com', 'www.criteo.com', 'account.t-mobile.com']
-urls = random.choices(urls, k=5)
+with open('scan_targets.txt') as f:
+    #x = [f.write(url) for url in urls]
+    urls = f.readlines()
+    urls = [line.strip() for line in urls]
+
+# trace()
+
+urls = random.choices(urls, k=1)
 
 async def main():
     start = time.perf_counter()
-    async with AsyncSession() as s:
-        tasks = [s.get(url, impersonate='chrome110') for url in urls]
+    async with AsyncSession() as s:    
+        tasks = [s.get(url, impersonate='chrome110', headers={'X-Bug-Bounty':'BugCrowd-jitheshkuyyalil'}) for url in urls]
 
         results = await asyncio.gather(*tasks)
 
         for result in results:
-            print(result.url, result.status_code)
+            print(result.url, result.status_code, result.headers)
             print(result.text, file=open('test' + '.html', 'w'))
 
         print(results)
