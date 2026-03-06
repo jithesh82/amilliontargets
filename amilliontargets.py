55fd22da4b51f2db9b24591777659c707727b4e2
jithesh82 <jithesh82@gmail.com>
2026-03-05 22:40:14 -0500

    update amilliontargets.py

--- amilliontargets.py
+++ amilliontargets.py
@@ -1,12 +1,12 @@
 """
-task1: parse html file for .js files
 taks2: recursively get all .js files
 """
 import asyncio
 from curl_cffi.requests import AsyncSession 
 import random
 import time
-#from pdb import set_trace as trace
+from linkfinder import myLinkFinder
+from pdb import set_trace as trace
 
 with open('scan_targets.txt') as f:
     #x = [f.write(url) for url in urls]
@@ -25,23 +25,50 @@ async def main():
         results = await asyncio.gather(*tasks)
 
         for result in results:
-            print(result.url, result.status_code, result.headers)
+            print(result.url, result.status_code)
             print(result.text, file=open('test' + '.html', 'w'))
 
         print(results)
 
         midtime = time.perf_counter()
 
-        async def contentWrite(result):
-            fName = result.url.split('/')[2] + '.html'
-            with open(fName, 'w') as f:
-                f.write(result.text)
+        async def analyzeHTML(result):
+            foundPattern = myLinkFinder('text ' + result.text, 'cli', '(?i)(API|secret|admin|administrator|internal|old|bak|backup|key|env|.env|back|bkup)')
+            print('*' * 15)
+            print(foundPattern)
+            print('*' * 15)
+            jslist = myLinkFinder('text ' + result.text, 'cli', '.js')
+            #print(patternFound)
+            jslistupdate = []
+            # change relative url to full url
+            for jslink in jslist:
+                if not jslink.startswith('http'):
+                    from urllib.parse import urljoin
+                    base_url = result.url  
+                    relative_url = jslink
+                    fullurl = urljoin(base_url, relative_url)
+                    jslistupdate.append(fullurl)
+                else:
+                    jslistupdate.append(jslink)
 
-        tasks = [contentWrite(result) for result in results]
+            return jslistupdate
 
-        results = await asyncio.gather(*tasks)
+        tasks = [analyzeHTML(result) for result in results]
+        jslist = await asyncio.gather(*tasks)
+        print('- -' * 15)
+        #jslist = jslist[0]
+        for domain in jslist:
+            [print(item) for item in domain]
+
+        #async def contentWrite(result):
+        #    fName = result.url.split('/')[2] + '.html'
+        #    with open(fName, 'w') as f:
+        #        f.write(result.text)
+
+        #tasks = [contentWrite(result) for result in results]
+
+        #results = await asyncio.gather(*tasks)
     end = time.perf_counter()
     print('time taken: %.2f, %.2f' % ((midtime - start), (end - midtime)))
-
 
 asyncio.run(main())
