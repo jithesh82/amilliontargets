1b4bb8b2986d33a58415bc2a6f7fc34fa133bf87
jithesh82 <jithesh82@gmail.com>
2026-03-16 01:33:36 -0400

    update amilliontargets.py

--- amilliontargets.py
+++ amilliontargets.py
@@ -2,11 +2,13 @@
 taks2: recursively get all .js files
 """
 import asyncio
+import pdb
 from curl_cffi.requests import AsyncSession 
 import random
 import time
 from linkfinder import myLinkFinder
 import aiosqlite
+import re
 #from pdb import set_trace as trace
 
 with open('scan_targets.txt') as f:
@@ -14,10 +16,10 @@ with open('scan_targets.txt') as f:
     urls = f.readlines()
     urls = [line.strip() for line in urls]
 
-urls = ["http://localhost:3000/"] 
+url = "http://localhost:3000/"
 # trace()
 
-urls = random.choices(urls, k=2)
+#urls = random.choices(urls, k=2)
 #urls = ["jitheshkuyyalil.com"]
 
 async def getUrls(urls: list) -> list: 
@@ -30,33 +32,46 @@ async def main():
     start = time.perf_counter()
     async with AsyncSession() as s:    
         async with aiosqlite.connect("db_amilliontargets.db") as db:
-            await db.execute("CREATE TABLE IF NOT EXISTS scan_results (id INTEGER PRIMARY KEY AUTOINCREMENT, url TEXT, status_code INTEGER)")   
-            tasks = [s.get(url, impersonate='chrome110', headers={'X-Bug-Bounty':'BugCrowd-jitheshkuyyalil'}) for url in urls]
+            await db.execute("CREATE TABLE IF NOT EXISTS scan_results (id INTEGER PRIMARY KEY AUTOINCREMENT, url TEXT, status_code INTEGER, matches TEXT)")   
+            #tasks = [s.get(url, impersonate='chrome110', headers={'X-Bug-Bounty':'BugCrowd-jitheshkuyyalil'}) for url in urls]
 
-            results = await asyncio.gather(*tasks)
+            result = await s.get(url, impersonate='chrome110', headers={'X-Bug-Bounty':'BugCrowd-jitheshkuyyalil'})
+
+            #results = await asyncio.gather(*tasks)
 
             #results = await getUrls(urls)
 
-            for result in results:
-                print(result.url, result.status_code)
+            print(result.url, result.status_code, result.text[:100])
+            #import pdb; pdb.set_trace()
+
+            #for result in results:
+            #    print(result.url, result.status_code)
             #    print(result.text, file=open('test' + '.html', 'w'))
 
-            print(results)
+            #print(results)
 
             midtime = time.perf_counter()
 
             async def analyzeHTML(result):
-                foundPattern = myLinkFinder('text ' + result.text, 'cli', '(?i)(admin)')
-                if foundPattern:
-                    with open('results_amilliontarget.txt', 'a') as f:
+                #foundPattern = myLinkFinder('text ' + result.text, 'cli', '(?i)(admin|administrator|internal|old|bak|backup|key|env|.env|back|bkup)')
+                pattern = re.compile(r'(?i)(admin|administrator|internal|old|bak|backup|key|env|.env|back|bkup)')
+                matches = pattern.findall(result.text)
+                #print(matches)
+                if matches:
+                    #with open('results_amilliontarget.txt', 'a') as f:
                         #f.write(result.url + '\n')
-                        await db.execute("INSERT INTO scan_results (url, status_code) VALUES (?, ?)", (result.url, result.status_code))
-                        await db.commit()
+                    print(matches)
+                    await db.execute("INSERT INTO scan_results (url, status_code, matches) VALUES (?, ?, ?)", (result.url, result.status_code, ', '.join(matches)))
+                    await db.commit()
+                    return matches
 
-            tasks = [analyzeHTML(result) for result in results]
+            matches = await analyzeHTML(result)    
 
-            htmlResults = await asyncio.gather(*tasks) 
-            del htmlResults 
+            #tasks = [analyzeHTML(result) for result in results]
+
+            #htmlResults = await asyncio.gather(*tasks) 
+            print(matches)
+            import pdb; pdb.set_trace() 
 
             async def getJS(result):
                 print('*' * 15)
