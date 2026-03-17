ab7503028d6113ca19d015399f4ea2aa3ea0de30
jithesh82 <jithesh82@gmail.com>
2026-03-16 22:36:24 -0400

    adding jslinkfinder.py

---
+++ jslinkfinder.py
@@ -0,0 +1,76 @@
+regex_str = r"""
+
+  (?:"|')                               
+
+  (
+    ((?:[a-zA-Z]{1,10}://|//)           
+    [^"'/]{1,}\.                        
+    [a-zA-Z]{2,}[^"']{0,})              
+
+    |
+
+    ((?:/|\.\./|\./)                    
+    [^"'><,;| *()(%%$^/\\\[\]]          
+    [^"'><,;|()]{1,})                   
+
+    |
+
+    ([a-zA-Z0-9_\-/]{1,}/               
+    [a-zA-Z0-9_\-/.]{1,}                
+    \.(?:[a-zA-Z]{1,4}|action)          
+    (?:[\?|#][^"|']{0,}|))             
+
+    |
+
+    ([a-zA-Z0-9_\-/]{1,}/               
+    [a-zA-Z0-9_\-/]{3,}                 
+    (?:[\?|#][^"|']{0,}|))             
+
+    |
+
+    ([a-zA-Z0-9_\-]{1,}                 
+    \.(?:php|asp|aspx|jsp|json|
+         action|html|js|txt|xml)        
+    (?:[\?|#][^"|']{0,}|))              
+
+  )
+
+  (?:"|')                               
+
+"""
+print(regex_str)
+
+import re
+
+from sympy import content
+
+with open('www.criteo.com.html') as f:
+    content = f.read()
+    #JS_PATTERN = re.compile(r'<script[^>]+src=["\']([^"\']+)["\']', re.IGNORECASE)
+    #JS_PATTERN = re.compile(regex_str)
+    # all_matches = [(m.group(1), m.start(0), m.end(0)); for m in re.finditer(regex_str, content)]
+    regex = re.compile(regex_str, re.VERBOSE)
+    items = [{"link": m.group(1)} for m in re.finditer(regex, content)]
+    no_dup = True
+    if no_dup:
+      # Remove duplication
+      all_links = set()
+      no_dup_items = []
+      for item in items:
+          if item["link"] not in all_links:
+              all_links.add(item["link"])
+              no_dup_items.append(item)
+      items = no_dup_items
+      #urls = JS_PATTERN.findall(content)
+    # Match Regex
+    filtered_items = []
+    more_regex = r"(?i)\.js$"
+    for item in items:
+        # Remove other capture groups from regex results
+        if more_regex:
+            if re.search(more_regex, item["link"]):
+                filtered_items.append(item)
+    
+    print(filtered_items)
+    jslist = [item["link"] for item in filtered_items]
+    #print(urls)
